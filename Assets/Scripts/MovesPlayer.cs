f2e8b73a9c6256472b196a72b87940364a5e77fd
Erdem Çobanoğlu <erdem.cobanoglu1635@gmail.com>
2020-12-26 19:11:01 +0300

    Pushed the prototype version

---
+++ Assets/Scripts/MovesPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovesPlayer : MonoBehaviour
+{
+    public PlayerMovement pm;
+    public float platformSpeed;
+    private void Start()
+    {
+        pm.setMovePlatformSpeed(platformSpeed);
+    }
+
+    void Update()
+    {
+        
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Player");
+            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+            pm.setMovePlatform(true);
+
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            pm.setMovePlatform(false);
+        }
+    }
+
+}
---
+++ Assets/Scripts/ScrollTexture.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollTexture : MonoBehaviour
+{
+    private Material material;
+    private float currentScroll;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<SpriteRenderer>().material;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentScroll += speed * Time.deltaTime;
+        material.mainTextureOffset = new Vector2 (currentScroll,0);
+    }
+}
